5b8601b7dee7d994f54c8be48e58375c694b0e98
jiaxing <jxwan98@gmail.com>
2023-05-28 23:12:41 +0800

    Part 3: Sort, Filter, Paging and Grouping

--- EFCoreTutorial/Pages/Students/Index.cshtml.cs
+++ EFCoreTutorial/Pages/Students/Index.cshtml.cs
@@ -12,21 +12,67 @@ namespace EFCoreTutorial.Pages.Students
 {
     public class IndexModel : PageModel
     {
-        private readonly EFCoreTutorial.Data.SchoolContext _context;
+        private readonly SchoolContext _context;
+		private readonly IConfiguration Configuration;
 
-        public IndexModel(EFCoreTutorial.Data.SchoolContext context)
+		public IndexModel(SchoolContext context, IConfiguration configuration)
+		{
+			_context = context;
+			Configuration = configuration;
+		}
+
+		public string NameSort { get; set; }
+        public string DateSort { get; set; }
+        public string CurrentFilter { get; set; }
+        public string CurrentSort { get; set; }
+
+		public PaginatedList<Student> Students { get; set; }
+
+        public async Task OnGetAsync(string sortOrder, string currentFilter, string searchString, int? pageIndex)
         {
-            _context = context;
-        }
+			CurrentSort = sortOrder;
+			NameSort = string.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+			DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+			
+			if (searchString != null)
+			{
+				pageIndex = 1;
+			}
+			else
+			{
+				searchString = currentFilter;
+			}
 
-        public IList<Student> Student { get;set; } = default!;
+			CurrentFilter = searchString;
 
-        public async Task OnGetAsync()
-        {
-            if (_context.Students != null)
+			IQueryable<Student> studentsIQ = from s in _context.Students
+                                             select s;
+
+			if (!string.IsNullOrEmpty(searchString))
+			{
+				studentsIQ = studentsIQ.Where(s => s.LastName.Contains(searchString)
+									   || s.FirstMidName.Contains(searchString));
+			}
+
+			switch (sortOrder)
             {
-                Student = await _context.Students.ToListAsync();
+                case "name_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.LastName);
+                    break;
+                case "Date":
+                    studentsIQ = studentsIQ.OrderBy(s => s.EnrollmentDate);
+                    break;
+                case "date_desc":
+                    studentsIQ = studentsIQ.OrderByDescending(s => s.EnrollmentDate);
+                    break;
+                default:
+                    studentsIQ = studentsIQ.OrderBy(s => s.LastName);
+                    break;
             }
+
+			var pageSize = Configuration.GetValue("PageSize", 4);
+			Students = await PaginatedList<Student>.CreateAsync(
+				studentsIQ.AsNoTracking(), pageIndex ?? 1, pageSize);
         }
     }
 }
--- readme.md
+++ readme.md
@@ -1,8 +1,15 @@
 # ***Razor Pages with Entity Framework Core in ASP.NET Core***  
 
-## *Get Started*
+## *Get Started*  
 ### https://learn.microsoft.com/en-us/aspnet/core/data/ef-rp/intro?view=aspnetcore-7.0&tabs=visual-studio  
 
 ## *Razor Pages with EF Core in ASP.NET Core - CRUD*  
 ### https://learn.microsoft.com/en-us/aspnet/core/data/ef-rp/crud?view=aspnetcore-7.0  
-1. TryUpdateModelAsync
+1. TryUpdateModelAsync  
+
+## *Razor Pages with EF Core in ASP.NET Core - Sort, Filter, Paging*  
+### https://learn.microsoft.com/en-us/aspnet/core/data/ef-rp/sort-filter-page?view=aspnetcore-7.0  
+1. Add sorting
+2. Add filtering
+3. Add paging
+4. Grouping
